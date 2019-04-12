f61fe93c3715335278bfd866809cb99e6985203f
letzer <letzercartagena@gmail.com>
2019-04-11 22:11:26 -0400

    Add IBusinessFragment lcn

--- intextmarket2/src/main/java/com/intext/intextmarket2/views/IBusinessFragment.java
+++ intextmarket2/src/main/java/com/intext/intextmarket2/views/IBusinessFragment.java
@@ -10,48 +10,35 @@ import android.view.ViewGroup;
 
 import com.intext.intextmarket2.R;
 
+/**
+ * Created by Ing. Letzer Cartagena Negron
+ * InTextChat @2019
+ * @author INTEXT SOFTWARE LLC
+ *
+ * Copyright (C) 2019 INTEXT SOFTWARE LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 public class IBusinessFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private IMarketBusinessListener iMarketBusinessListener;
 
-    private OnFragmentInteractionListener mListener;
+    public interface IMarketBusinessListener {
 
-    public IBusinessFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment IBusinessFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static IBusinessFragment newInstance(String param1, String param2) {
-        IBusinessFragment fragment = new IBusinessFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public IBusinessFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,42 +47,21 @@ public class IBusinessFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_ibusiness, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof IMarketBusinessListener) {
+            iMarketBusinessListener = (IMarketBusinessListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement IMarketBusinessListener");
         }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        iMarketBusinessListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
